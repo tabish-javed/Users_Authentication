228750d96ac2f6ffceb408ac3eea88e50ebb429b
TABISH <tabishjaved@hotmail.com>
2023-02-09 18:30:47 +0530

    improved user experience

--- routes/demo.js
+++ routes/demo.js
@@ -31,7 +31,19 @@ router.get('/signup', function (req, res) {
 
 
 router.get('/login', function (req, res) {
-    res.render('login');
+    let sessionInputData = req.session.inputData
+
+    if (!sessionInputData) {
+        sessionInputData = {
+            hasError: false,
+            email: "",
+            password: ""
+        }
+    }
+
+    // Clear previous session data before rendering signup page
+    req.session.inputData = null
+    res.render('login', { inputData: sessionInputData });
 });
 
 
@@ -71,8 +83,19 @@ router.post('/signup', async function (req, res) {
     // If email address exists in our database, then do not proceed and
     // redirect user back to signup page.
     if (existingUser) {
-        console.log("User already registered")
-        return res.redirect("/signup")
+        // Save session data if user already exists
+        req.session.inputData = {
+            hasError: true,
+            message: "User already exists!",
+            email: enteredEmail,
+            confirmEmail: enteredConfirmEmail,
+            password: enteredPassword
+        }
+        // Save session data
+        req.session.save(function () {
+            res.redirect("/signup")
+        })
+        return
     }
     // Finally hash the given password
     const hashedPassword = await bcrypt.hash(enteredPassword, 12)
@@ -110,32 +133,60 @@ router.post('/login', async function (req, res) {
     const existingUser = await db.getDb().collection("users").findOne({ email: enteredEmail })
     // And if user's email doesn't exists/registered in database, redirect to login again
     if (!existingUser) {
-        console.log("This email address isn't registered with us")
-        return res.redirect("/login")
+        req.session.inputData = {
+            hasError: true,
+            message: "Logon Error! please check your credentials",
+            email: enteredEmail,
+            password: enteredPassword
+        }
+        req.session.save(function () {
+            res.redirect("/login")
+        })
+        return
     }
     // Otherwise, check if user's parsed password is same as in our database
     const passwordsAreEqual = await bcrypt.compare(enteredPassword, existingUser.password)
     // If password isn't equal to the one stored in database, redirect to login again
     if (!passwordsAreEqual) {
-        console.log("Could not log in - incorrect password")
-        return res.redirect("/login")
+        req.session.inputData = {
+            hasError: true,
+            message: "Logon Error!, please check your credentials",
+            email: enteredEmail,
+            password: enteredPassword
+        }
+        req.session.save(function () {
+            res.redirect("/login")
+        })
+        return
     }
     // Add data to session
     req.session.user = { id: existingUser._id, email: existingUser.email }
     req.session.isAuthenticated = true
     req.session.save(function () {
-        res.redirect("/admin")
+        res.redirect("/profile")
     })
     // Finally user get authenticated, passing all above conditions
     console.log("User is authenticated")
 });
 
 
-router.get('/admin', function (req, res) {
+router.get('/admin', async function (req, res) {
     if (!req.session.isAuthenticated) { // if (!req.session.user)
         return res.status(401).render("401")
     }
+    const user = await db.getDb().collection("users").findOne({ _id: req.session.user.id })
+    if (!user || !user.isAdmin) {
+        return res.status(403).render("403")
+    }
     res.render('admin');
+});
+
+
+router.get('/profile', function (req, res) {
+    if (!req.session.isAuthenticated) { // if (!req.session.user)
+        return res.status(401).render("401")
+    }
+    res.render('profile');
 });
 
 
